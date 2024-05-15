54954e2173bbc92580d57d1bf82d70166c4f1a76
kimherdz <kimberly.hernandez@galileo.edu>
2024-05-15 02:31:00 -0600

    Tutorial 2 Listo

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import "./index.css";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import Contact, { loader as contactLoader, } from "./routes/contact";
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
+import EditContact, { action as editAction, } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 const router = createBrowserRouter([
   {
@@ -14,9 +17,22 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
       },
     ],
   },
